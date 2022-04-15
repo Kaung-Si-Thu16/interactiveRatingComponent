55f21ef28635cd48cc7d7be4f5018ddea8178fd5
Kaung-Si-Thu16 <89345859+Kaung-Si-Thu16@users.noreply.github.com>
2022-04-15 17:16:29 +0630

    UI finished

---
+++ script.js
@@ -0,0 +1,7 @@
+const button1 = document.querySelector("#btn1");
+const button2 = document.querySelector("#btn2");
+const button3 = document.querySelector("#btn3");
+const button4 = document.querySelector("#btn4");
+const button5 = document.querySelector("#btn5");
+
+const buttons = [button1, button2, button3, button4, button5];
