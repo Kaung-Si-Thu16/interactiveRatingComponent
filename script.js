3bd9db09074b6b6cfcb842eb4cb82a8f57f5fba7
Kaung-Si-Thu16 <89345859+Kaung-Si-Thu16@users.noreply.github.com>
2022-04-15 20:44:15 +0630

    made some changes

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ const button4 = document.querySelector("#btn4");
 const button5 = document.querySelector("#btn5");
 
 const buttons = [button1, button2, button3, button4, button5];
+
+const clickedBtn1 = () => {
+    console.log(button1.name);
+}
+
+const clickedBtn2 = () => {
+    console.log(button2.name);
+}
+
+const clickedBtn3 = () => {
+    console.log(button3.name);
+}
+
+const clickedBtn4 = () => {
+    console.log(button4.name);
+}
+
+const clickedBtn5 = () => {
+    console.log(button5.name);
+}
+
+const gg = () => {
+    const mainPage = document.querySelector(".container");
+    mainPage.style.display = "none";
+
+    console.log("Hello World")
+}
